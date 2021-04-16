55a035582e66f230f1f02bdc102d351b65a9686e
developer <team-developer@gmail.com>
2021-04-16 13:35:43 +0800

    upd: integrated babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-modulr.exports = function () {
+/** */
+
+module.exports = function (api, options, dirname) {
+  api.cache(true);
+  console.log(options, dirname);
+  const presets = [
+    [
+      "@babel/env",
+      {
+        targets: {
+          edge: "17",
+          firefox: "60",
+          chrome: "67",
+          safari: "11.1",
+        },
+        useBuiltIns: "usage",
+        corejs: "3.10.1",
+      },
+    ],
+  ];
+  const plugins = ["@babel/transform-arrow-functions"];
+
   return {
-    presets: [],
-    plugins: [],
+    presets,
+    plugins,
   };
 };
--- dev.sh
+++ dev.sh
@@ -1,6 +1,44 @@
 #!/usr/bin/env bash
 
-echo 'start development mode.'
-nohup yarn eid &
-nohup yarn demo &
+# ------- Defined config -----------
+PROC_EID_NAME=eidifr
+PROC_DEMO_NAME=eidSign
+
+SEVSER_BIN="lite-server"
+dir=$(cd "$(dirname "$0")";pwd)
+
+
+function startIfrServer() {
+  echo $dir
+}
+
+
+function start(){
+  nohup ./node_modules/lite-server/bin/lite-server -c ./ci/bs-eid-config.js $PROC_EID_NAME 2>&1 &
+}
+
+
+function stopIfrServer(){
+  echo shutdown $PROC_EID_NAME
+  ps aux |grep $PROC_EID_NAME |grep -v grep |awk '{print "kill -9 " $2}'|sh
+  return 0
+}
+
+function stop(){
+  A=`stopIfrServer`
+  echo $A
+}
+
+case $1 in
+"start")
+  start
+  ;;
+"stop")
+  stop
+  ;;
+*)
+  echo 'entry start,stop or restart'
+  ;;
+esac
+
 
